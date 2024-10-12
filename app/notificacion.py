7783e4a20744848963f52dfdc40da2213dc24326
David <davidcuartashernandez@gmail.com>
2024-10-12 15:49:59 -0500

    -

--- app/notificacion.py
+++ app/notificacion.py
@@ -2,6 +2,18 @@ class Notificacion:
     def enviar_notificacion(self, metodo, mensaje):
         metodo.enviar_notificacion(mensaje)
 
+class Correo(Notificacion):
+    def enviar_notificacion(self, mensaje):
+        print(f"Enviando correo: {mensaje}")
+
+class Celular(Notificacion):
+    def enviar_notificacion(self, mensaje):
+        print(f"Enviando mensaje de texto: {mensaje}")
+
+class Aplicacion(Notificacion):
+    def enviar_notificacion(self, mensaje):
+        print(f"Enviando notificación en la aplicación: {mensaje}")
+
 class WhatsApp(Notificacion):
     def enviar_notificacion(self, mensaje):
         print(f"Enviando mensaje de WhatsApp: {mensaje}")
