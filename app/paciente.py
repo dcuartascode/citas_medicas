f94fc60c6fdfffff4b97ddd441f31bc7b9be3a61
David <davidcuartashernandez@gmail.com>
2024-10-12 15:49:05 -0500

    -

--- app/paciente.py
+++ app/paciente.py
@@ -1,28 +1,11 @@
-from agenda import Agenda
-from cita import Cita
 from persona import Persona
 
 class Paciente(Persona):
     def __init__(self, identificacion, nombre, celular, correo):
-        super().__init__(identificacion, nombre, celular)
-        self.correo = correo
+        super().__init__(identificacion, nombre, celular, correo)
         self.medico_preferencia = None
-        self.agenda = Agenda()
 
     def asignar_medico_preferencia(self, medico):
         self.medico_preferencia = medico
         print(f"El médico {medico.nombre} ha sido asignado como preferencia para el paciente {self.nombre}")
 
-    def pedir_cita(self, medico, fecha, motivo):
-        if medico.verificar_disponibilidad(fecha):
-            nueva_cita = Cita(self, medico, fecha)
-            medico.agenda.agregar_cita(nueva_cita)
-            self.agenda.agregar_cita(nueva_cita)
-            print(f"Cita solicitada para el {fecha} con el Dr. {medico.nombre}")
-        else:
-            print(f"No hay disponibilidad con el Dr. {medico.nombre} en la fecha {fecha}")
-
-    def cancelar_cita(self, cita):
-        self.agenda.cancelar_cita(cita)
-        cita.cancelar_cita()  
-        print(f"Cita cancelada {self.nombre}")
--- app/persona.py
+++ app/persona.py
@@ -1,5 +1,6 @@
 class Persona:
-    def __init__(self, identificacion, nombre, celular):
+    def __init__(self, identificacion, nombre, celular, correo):
         self.identificacion = identificacion
         self.nombre = nombre
         self.celular = celular
+        self.correo = correo
